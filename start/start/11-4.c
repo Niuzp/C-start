ced8e74c630067a917d7ae4cfda25f6e9e5f3b79
Niuzp <1659445154@qq.com>
2024-11-04 22:17:03 +0800

    first commit

---
+++ start/start/11-4.c
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	printf("hello, world\n");
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+
+	return 1;
+}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int sum = a + b;
+//	printf("%d",sum);
+//	return 0;
+//}
+
+//enum Color
+//{
+//	RED,
+//	GREEN,
+//	BLUE
+//};
+//
+//int main()
+//{
+//	int c = RED;
+//	printf("%d", c);
+//}
+
+//11.4
+// ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+//廣瞥
+
+/*
+c囂冱廣瞥欠鯉
+*/
+
+//C++廣瞥欠鯉
+
+//int Add(int x,int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+
+//int main()
+//{
+	//int input = 0;
+	//scanf('僉夲\n',&input);
+	////登僅囂鞘
+	//if (input == 1)
+	//{
+	//	printf('僉夲阻1\n');
+	//}
+	//else
+	//{
+	//	printf('短嗤恂竃僉夲\n');
+	//}
+
+	////while儉桟囂鞘
+	//int line = 0;
+	//while (line < 20000)
+	//{
+	//	printf("亟旗鷹��%d\n", line);
+	//	line++;
+	//}
+
+	//痕方
+	//int n1 = 0;
+	//int n2 = 0;
+	//scanf("%d %d",&n1, &n2);
+	//int sum = Add(n1, n2);
+	//printf("%d\n", sum);
+	//return 0;
+//}
+
+//方怏
+//int arr[10] = { 1,2,3,4,4 };
+
+// ！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
